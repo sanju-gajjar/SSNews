88b562c66d6594a52c0ab176abeb4afd26053457
sanju gajjar <53038038+sanju-gajjar@users.noreply.github.com>
2025-03-30 23:25:26 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 app.use(cors())
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://swadeshsandesh:2t9Z4PmygBU41RYV@clusterssn.rasxlii.mongodb.net/?retryWrites=true&w=majority&appName=ClusterSSN', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Could not connect to MongoDB', err));
 
